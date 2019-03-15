39347b51c446a90d596836b6e9e36882decfd15a
elflautero <elflautero@gmail.com>
2019-03-14 23:46:22 -0300

    1

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -2,13 +2,39 @@ package com.mycompany.myapp;
 
 import android.app.*;
 import android.os.*;
+import android.webkit.*;
+import android.view.*;
 
 public class MainActivity extends Activity 
-{
+{ 
+	WebView wv;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		
+		wv = (WebView)
+			findViewById(R.id.wv_id);
+		WebSettings webSettings = wv.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+			
+		String frameVideo = ("<iframe width=100% height='200' src='https://www.youtube.com/embed/YR5ApYxkU-U' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
+		wv.loadData(frameVideo, "text/html", "utf-8");
+			
+		
+		/*
+		wv.loadUrl("https://m.youtube.com");
+		WebSettings webSettings = wv.getSettings();
+		//Habilitando o JavaScript
+		webSettings.setJavaScriptEnabled(true);*/
     }
+	
+	public void onClickSetLinkWebMap(View view){
+
+		//finish();
+		wv.loadUrl("https://www.google.com.br/search?q=significado+de+saneamento&sourceid=chrome-mobile&ie=UTF-8&inm=vs#tts=0");
+		
+	}
 }
+
