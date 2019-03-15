200cdff8777fa781d12bcea45c1481e419c20fe6
elflautero <elflautero@gmail.com>
2019-03-15 02:02:26 -0300

    Inclusao de uma slide bar e seu metodo

--- app/src/main/java/com/mycompany/myapp/MainActivity.java
+++ app/src/main/java/com/mycompany/myapp/MainActivity.java
@@ -4,37 +4,63 @@ import android.app.*;
 import android.os.*;
 import android.webkit.*;
 import android.view.*;
+import android.widget.*;
+import android.widget.SeekBar.*;
 
 public class MainActivity extends Activity 
 { 
 	WebView wv;
+	SeekBar seekB;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+		initializeVariables ();
 		
 		wv = (WebView)
-			findViewById(R.id.wv_id);
+			findViewById(R.id.webV_id);
 		WebSettings webSettings = wv.getSettings();
 		webSettings.setJavaScriptEnabled(true);
 			
-		String frameVideo = ("<iframe width=100% height='200' src='https://www.youtube.com/embed/YR5ApYxkU-U' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
+		String frameVideo = ("<iframe width=100% height='300' src='https://www.youtube.com/embed/YR5ApYxkU-U' frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>");
 		wv.loadData(frameVideo, "text/html", "utf-8");
-			
 		
-		/*
-		wv.loadUrl("https://m.youtube.com");
-		WebSettings webSettings = wv.getSettings();
-		//Habilitando o JavaScript
-		webSettings.setJavaScriptEnabled(true);*/
-    }
+		seekB = (SeekBar) findViewById(R.id.seekB_id);
+		seekB.setProgress(1);
+		
+		
+	    seekB.setOnSeekBarChangeListener(new OnSeekBarChangeListener(){
+			
+			int progress = 0;
+			
+			public void onProgressChanged(
+					SeekBar sb,int progressValue, 
+					boolean fromUser)
+				{
+				Toast.makeText(
+					getApplicationContext(), 
+					"Started tracking seekbar"+progressValue, 
+					Toast.LENGTH_SHORT).show();
+				}
+			public void  onStartTrackingTouch(SeekBar sb){
+				
+			}
+			public  void onStopTrackingTouch(SeekBar sb){
+				
+			}
+			
+			
+		});
 	
-	public void onClickSetLinkWebMap(View view){
+			}
 
-		//finish();
-		wv.loadUrl("https://www.google.com.br/search?q=significado+de+saneamento&sourceid=chrome-mobile&ie=UTF-8&inm=vs#tts=0");
+	public void initializeVariables () {
+
 		
 	}
+		
+	
 }
 
